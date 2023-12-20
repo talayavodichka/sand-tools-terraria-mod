0ae70f9e84b1adcdd44ae90b1375f596ef15ae81
Alex <iamkidandyouknowit@gmail.com>
2023-12-20 19:50:08 +0500

    1.2.0

---
+++ Buffs/PoopyMonsterBuff.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using SandTools.Minions;
+
+namespace SandTools.Buffs
+{
+    internal class PoopyMonsterBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<PoopyMonster>()] > 0)
+            {
+                player.buffTime[buffIndex] = 18000;
+                return;
+            }
+
+            player.DelBuff(buffIndex);
+            buffIndex--;
+        }
+    }
+}
---
+++ Items/SandPoopSummonStaff.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using SandTools.Buffs;
+using SandTools.Minions;
+
+namespace SandTools.Items
+{
+    public class SandPoopSummonStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+            ItemID.Sets.GamepadWholeScreenUseRange[Type] = true;
+            ItemID.Sets.LockOnIgnoresCollision[Type] = true;
+
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item44;
+
+            Item.DamageType = DamageClass.Summon;
+            Item.damage = 100000;
+            Item.knockBack = 20;
+            Item.mana = 5;
+            Item.noMelee = true;
+            Item.value = 0;
+            Item.rare = 0;
+
+            Item.shoot = ModContent.ProjectileType<PoopyMonster>();
+            Item.buffType = ModContent.BuffType<PoopyMonsterBuff>();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.SandBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+            return false;
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = Main.MouseWorld;
+        }
+    }
+}
